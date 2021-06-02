678d1b5a6e1171872e865518b51a645bf50ddd7e
Baruch-Karlin <bukbeak@gmail.com>
2021-06-02 15:47:15 +0300

    get all emojis

--- Backend/routes/emoji/emoji.js
+++ Backend/routes/emoji/emoji.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Emoji = require('./mongoose_schema/emoji');
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send(`Hello world from pictures`);
+router.get('/', async (req, res, next) => {
+    const emojis = await Emoji.find()
+    res.status(200).send(emojis);
 });
 
 module.exports = router;
